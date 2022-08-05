c8d5307aed91851a0b18aa37c71f2032c661fbbc
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-08-05 16:23:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 resource "aws_s3_bucket" "onebucket" {
   bucket = var.bucket_name 
   acl = var.acl_value
-
-  lifecycle_rule {
-    prefix  = var.environment_key
-    enabled = true
-  }
+  bucket_prefix  = var.environment_key
 }
